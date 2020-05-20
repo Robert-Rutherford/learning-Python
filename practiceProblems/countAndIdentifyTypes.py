5244dae7edae75e0867e35ec2856c99de2ea6bda
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-05-20 14:10:39 -0500

    python unfinished

---
+++ practiceProblems/countAndIdentifyTypes.py
@@ -0,0 +1,11 @@
+# Count and Identify Data Types
+# site: https://edabit.com/challenge/HXkpnCxJgxkFwaReT
+# Given a function that accepts unlimited arguments, check and count how many data types are in those arguments. Finally return the total in a list.
+#
+# List order is:
+#
+# [int, str, bool, list, tuple, dictionary]
+
+
+def count_datatypes(*args):
+	typecount = 
