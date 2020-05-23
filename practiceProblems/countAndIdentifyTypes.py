a418f5410b1ffdcac5ffad03b54721cea37c7941
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-05-23 12:59:32 -0500

    python identify and count type (variable arguments)

--- practiceProblems/countAndIdentifyTypes.py
+++ practiceProblems/countAndIdentifyTypes.py
@@ -8,4 +8,25 @@
 
 
 def count_datatypes(*args):
-	typecount = 
+    typecount = [0, 0, 0, 0, 0, 0]
+    for arg in args:
+        argtype = type(arg)
+        if argtype is int:
+            typecount[0] += 1
+        elif argtype is str:
+            typecount[1] += 1
+        elif argtype is bool:
+            typecount[2] += 1
+        elif argtype is list:
+            typecount[3] += 1
+        elif argtype is tuple:
+            typecount[4] += 1
+        elif argtype is dict:
+            typecount[5] += 1
+    return typecount
+
+
+print(count_datatypes(1, 45, "Hi", False))
+print(count_datatypes([10, 20], ("t", "Ok"), 2, 3, 1))
+print(count_datatypes("Hello", "Bye", True, True, False, {"1": "One", "2": "Two"}, [1, 3], {"Brayan": 18}, 25, 23))
+print(count_datatypes(4, 21, ("ES", "EN"), ("a", "b"), False, [1, 2, 3], [4, 5, 6]))
