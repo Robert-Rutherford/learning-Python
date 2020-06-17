db31ac611854986cc2e38cda02e79b0d7fa6afc2
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-06-17 14:45:17 -0500

    python dog daycare troubles

---
+++ practiceProblems/BeginningAndEndPairs.py
@@ -0,0 +1,10 @@
+# Beginning and End Pairs
+# site: https://edabit.com/challenge/HrCuzAKE6skEYgDmf
+# Write a function that pairs the first number in an array with the last, the second number with the second to last,
+# etc.
+# notes:
+# If the list has an odd length, repeat the middle element twice for the last pair.
+# Return an empty list if the input is an empty list.
+
+
+def pairs(lst):
