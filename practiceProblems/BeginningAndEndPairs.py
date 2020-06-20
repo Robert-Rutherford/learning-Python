d4e24b28470a71f9ea70990a6bcfcc9dd6158b5b
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-06-20 14:09:59 -0500

    python beginning end pairs finished

--- practiceProblems/BeginningAndEndPairs.py
+++ practiceProblems/BeginningAndEndPairs.py
@@ -8,3 +8,30 @@
 
 
 def pairs(lst):
+    pairedlist = list()
+    split = 0
+    reverse = -1
+    odd = False
+    if len(lst) % 2 == 0:
+        split = int(len(lst)/2)
+    else:
+        odd = True
+        split = int(int(len(lst))/2)
+    for x in range(split):
+        pairset = list()
+        pairset.append(lst[x])
+        pairset.append(lst[reverse])
+        reverse -= 1
+        pairedlist.append(pairset)
+    if odd:
+        oddset = list()
+        oddset.append(lst[split])
+        oddset.append(lst[split])
+        pairedlist.append(oddset)
+    return pairedlist
+
+
+print(pairs([1, 2, 3, 4, 5, 6, 7]))
+print(pairs([1, 2, 3, 4, 5, 6]))
+print(pairs([5, 9, 8, 1, 2]))
+print(pairs([]))
